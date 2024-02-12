1658c689d9189c6ae40b75f8299fab1dcf42154f
Alsaad <mrrgx.22@gmail.com>
2024-02-12 10:58:11 +0300

    update some views and add navigations

--- App.js
+++ App.js
@@ -1,22 +1,56 @@
 import React from "react";
+import { View, Text, StyleSheet, Button , TouchableOpacity} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SecondView from "./Views/SecondView";
 
-import { NavigationContainergit } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeView from "./Views/HomeView"
-import SecondView from "./Views/SecondView"
-import SpashView from "./Views/SplashView"
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+function HomeScreen({ navigation }) {
+  return (
+    <View style={style.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+
+function NewScreen({navigation}){
   return(
-      <NavigationContainer >
-        <Stack.Navigator>
-          <Stack.Screen name=" " component={SpashView}/>
-          <Stack.Screen name="SecondView" component={SecondView}/> 
-        </Stack.Navigator>
-      </NavigationContainer>
-  ); 
+    <View>
+      <Text>Hello , i am (NewScreen)</Text>
+      <Button title="Next" 
+        onPress={()=> navigation.navigate("New")}
+      ></Button>
+    </View>
+  );
 };
 
 
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={SecondView} />
+        <Stack.Screen name="New" component={NewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+const style = StyleSheet.create({
+  container:{
+    flex: 1, 
+    alignItems: 'center',
+    justifyContent: 'center' 
+  }
+});
+
+export default App;
